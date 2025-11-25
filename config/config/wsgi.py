6a92ff070847f3c3c5885a134e4a3427c132c2ba
DaraSynth <palizseo.mehrali@gmail.com>
2025-11-25 15:06:07 +0330

    Final attempt to fix all config path issues

--- config/config/wsgi.py
+++ config/config/wsgi.py
@@ -1,15 +1,14 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+
+# مسیر Root Directory پروژه را به sys.path اضافه می‌کند تا Django تنظیمات را پیدا کند.
+# اگر فایل wsgi.py در config/wsgi.py باشد، این خط یک پله به بالا (پوشه اصلی django) می‌رود.
+# اگر ریشه پروژه شما پوشه config است، این کد صحیح است.
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR))
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
--- django ordokhani vscode/myproject/challenges/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.days_list, name='days_list'),
-    path('<int:day>', views.dynamic_days_by_number),
-    path('<str:day>', views.dynamic_days, name='days-of-week'),
-]
